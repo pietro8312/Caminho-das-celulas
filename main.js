cccb678d1884273309f3c0a9a4c9171af5d86438
pietrodonicht8@gmail.com <pietrodonicht8@gmail.com>
2025-08-25 01:09:34 -0300

    (bild): add side menu

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 const bntHam = document.querySelector("#hamburger");
 var turn = false;
 
+const sideMenu = document.querySelector("section#side-menu");
+
+function showSideMenu (flag){
+    if(flag === false){
+        sideMenu.classList.remove("some")
+        sideMenu.style.transform = "translateX(-100%)";
+    }else{
+        sideMenu.classList.add("some")
+        sideMenu.style.transform = "translateX(100%)";
+    }
+}
+
 bntHam.addEventListener("mousedown", function(){
     if(turn === false){
         bntHam.style.rotate = "90deg";
 
-        //showSideMenu(turn);       adicionar quando fazer a function
+        showSideMenu(turn);
 
         turn = !turn;
     }else{
         bntHam.style.rotate = "0deg";
         
-        //showSideMenu(turn);       adicionar quando fazer a function
+        showSideMenu(turn);
         
         turn = !turn;
     }
